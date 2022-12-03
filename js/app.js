f7ee4b6fa16185584d2b61c8b2d151c01705baeb
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2022-12-03 16:54:05 +0100

    Added the category buttons and fixed the site width calc

--- js/app.js
+++ js/app.js
@@ -12,6 +12,26 @@ const renderMenu = async () => {
     renderMain(data[0].name);
 }
 
+// Renders the main content
+const renderMain = planet => {
+    document.querySelector('.content-description').innerHTML = planetData.filter(item => item.name === planet).map(content => `
+        <h1 class="title">${content.name}</h1>
+        <p class="description">${content.overview.content}</p>
+        <span class="source-link">Source : 
+            <a href="${content.overview.source}">Wikipedia</a>
+            <img alt="Read more on Wikipedia" src="assets/icon-source.svg">
+        </span>
+        <div class="category">
+            <button class="category-button selected" style="--color-selected: ${content.color}">
+                <span>01</span>Overview
+            </button>
+            <button class="category-button" style="--color-selected: ${content.color}">
+                <span>02</span>Internal structure
+            </button>
+        </div>
+    `).join('');
+}
+
 // Click the main menu
 document.querySelector('nav').addEventListener('click', (e) => {
     e.preventDefault();
@@ -20,11 +40,11 @@ document.querySelector('nav').addEventListener('click', (e) => {
     }
 })
 
-// Renders the main content
-const renderMain = planet => {
-    document.querySelector('.textcontent').innerHTML = planetData.filter(item => item.name === planet).map(content => `
-        <p>${content.overview.content}</p>
-    `).join('');
-}
+// Click the category button
+document.querySelector('.content-description').addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON') {
+        console.log('YEP');
+    }
+})
 
 renderMenu();
